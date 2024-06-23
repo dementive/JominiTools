fb32f061b58c98de5dab129730a8b294485c288c
dementive <87823030+dementive@users.noreply.github.com>
2024-06-23 03:40:32 -0400

    finish up plugin manager

--- src/plugin_manager.py
+++ src/plugin_manager.py
@@ -4,45 +4,31 @@ Class to handle generic tasks related to maintenance and updating of a sublime p
 
 import os
 import subprocess
+from typing_extensions import LiteralString
 
 
 class PluginManager:
-    def auto_update_plugin(self, path, branch="main"):
+    def __init__(self, path_to_repository: str, repository_url: LiteralString):
+        self.path_to_repository = path_to_repository
+        self.repository_url = repository_url
+
+    def auto_update_plugin(self, branch="main"):
         # Pull the latest changes from git to update a plugin on a certain branch
-        if not os.path.isdir(os.path.join(path, ".git")):
-            print(f"JominiTools: {path} is not a git repository.")
+        if not os.path.isdir(os.path.join(self.path_to_repository, ".git")):
+            print(f"JominiTools: {self.path_to_repository} is not a git repository.")
             return
 
         try:
-            subprocess.run(["git", "-C", path, "fetch"], check=True)
-            subprocess.run(["git", "-C", path, "checkout", branch], check=True)
+            subprocess.run(["git", "-C", self.path_to_repository, "fetch"], check=True)
+            subprocess.run(
+                ["git", "-C", self.path_to_repository, "checkout", branch], check=True
+            )
         except subprocess.CalledProcessError as e:
             print(f"JominiTools: Failed to switch to the {branch} branch: {e}")
             return
 
-        # Pull the latest changes
         try:
-            subprocess.run(["git", "-C", path, "pull"], check=True)
-            print(f"JominiTools: Pulled changes for {path}")
+            subprocess.run(["git", "-C", self.path_to_repository, "pull"], check=True)
+            print(f"JominiTools: Pulled changes for {self.path_to_repository}")
         except subprocess.CalledProcessError as e:
             print(f"JominiTools: Failed to update the {branch} branch: {e}")
-
-    def clone_and_update_repo(self, repo_url, destination_dir):
-        # Check if the.git directory exists in the destination directory, if it doens't clone it and switch to main.
-        if not os.path.exists(os.path.join(destination_dir, ".git")):
-            print(f"Cloning repository {repo_url} into {destination_dir}...")
-            subprocess.run(["git", "clone", repo_url, destination_dir], check=True)
-
-        subprocess.run(["git", "-C", destination_dir, "checkout", "main"], check=True)
-        subprocess.run(["git", "-C", destination_dir, "pull"], check=True)
-
-
-# Example usage
-# folder_path1 = sublime.packages_path() + f'{os.sep}Victoria3Tools'
-# sublime.set_timeout_async(
-# 	lambda: auto_update_plugin(folder_path1), 0
-# )
-# folder_path = sublime.packages_path() + f'{os.sep}ImperatorTools'
-# sublime.set_timeout_async(
-# 	lambda: auto_update_plugin(folder_path), 0
-# )
